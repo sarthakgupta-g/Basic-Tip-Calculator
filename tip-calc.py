a421c1900d3f7567d9625468883e24c6b8fe981d
Sarthak Gupta <sarthakgupta@umass.edu>
2026-08-15 17:36:46 -0400

    Fix tip calculation using correct variable name

--- tip-calc.py
+++ tip-calc.py
@@ -1,6 +1,6 @@
 bill=int(input("How much was your bill?"))
 tip_percent=int(input("What percent tip do you want to pay?"))
-tip=tip*bill*0.01
+tip=tip_percent*bill*0.01
 total=tip+bill
 print("Your tip is: $"+tip)
 print("Your total is: $"+total)

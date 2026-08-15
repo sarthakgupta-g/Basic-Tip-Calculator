56145d1feb893ebc6bfe59bbe7c6a393a7ce5185
Sarthak Gupta <sarthakgupta@umass.edu>
2026-08-15 17:45:51 -0400

    Change input types to float for bill and tip percent
    - Changing inputs to floating values
    - Adding formatted strings for clarity

--- tip-calc.py
+++ tip-calc.py
@@ -1,6 +1,8 @@
-bill=int(input("How much was your bill?"))
-tip_percent=int(input("What percent tip do you want to pay?"))
+bill=float(input("How much was your bill?"))
+tip_percent=float(input("What percent tip do you want to pay?"))
+
 tip=tip_percent*bill*0.01
 total=tip+bill
-print("Your tip is: $"+tip)
-print("Your total is: $"+total)
+
+print(f"Your tip is: ${tip:.2f}")
+print(f"Your total is: ${total:.2f}")
